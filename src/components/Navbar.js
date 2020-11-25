2fa99d8852195e56e0b7a695ad2f0f7723c53d22
andyvdoan <andyy.doan@gmail.com>
2020-11-25 16:27:35 -0600

    Added some structure to about modal

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,9 @@
+// G L O B A L   I M P O R T S //
 import React, { useState } from 'react';
 import Modal from 'react-modal';
 
+
+// M E D I A   I M P O R T S //
 import img from '../media/images/andy_headshot.jpeg'
 
 Modal.setAppElement("#root");
@@ -25,11 +28,11 @@ const Navbar = () => {
             <div className="about-section">
               <div className="head-section">
                 <h1 className="greeting">Hello,</h1>
-                <h2 className="greeting2">I'm Andy <span className="highlight">Van Doan</span></h2>
-                <p className="desc">I'm a Chicago-based software engineer.</p>
-                <p className="desc">Full-stack savvy and adept at designing/building engaging, performant applications.</p>
-                <p className="desc">Innovation and automation energize me.</p>
-                <p className="desc">If you have any cool projects or opportunities, get in touch!</p>
+                <h1 className="greeting2">I'm Andy <span className="highlight">Van Doan.</span></h1>
+                <p className="desc">I'm a Chicago-based, full-stack savvy software engineer.</p>
+                <p className="desc">I design and build engaging, performant applications.</p>
+                <p className="desc">Machine learning and automation energize me.</p>
+                <p className="desc">If you have any cool projects or opportunities, let's work together!</p>
               </div>
               <div className="subsection">
                 <h3>Education</h3>
@@ -40,8 +43,60 @@ const Navbar = () => {
                 <p className="subdesc">Text text text</p>
               </div>
             </div>
-            <div className="about-section">
+            <div className="about-section2">
               <img src={img} className="headshot"/>
+              <div className="techstack">
+                <div className="type">
+                  <div className="icons">
+                    <div className="single-icon">
+                  <h2 className="title">Recent Front-end</h2>
+                      <div className="icon">
+                      </div>
+                    <div className="single-icon">
+                      <div className="icon">
+                      </div>
+                    </div>
+                    <div className="single-icon">
+                      <div className="icon">
+                      </div>
+                    </div>
+                    <div className="single-icon">
+                      <div className="icon">
+                      </div>
+                    </div>
+                    <div className="single-icon">
+                      <div className="icon">
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div className="type">
+                    <div className="icons">
+                      <div className="single-icon">
+                  <h2 className="title">Recent Back-end</h2>
+                        <div className="icon">
+                        </div>
+                      </div>
+                      <div className="single-icon">
+                        <div className="icon">
+                        </div>
+                      </div>
+                      <div className="single-icon">
+                        <div className="icon">
+                        </div>
+                      </div>
+                      <div className="single-icon">
+                        <div className="icon">
+                        </div>
+                      </div>
+                      <div className="single-icon">
+                        <div className="icon">
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
         </Modal>
